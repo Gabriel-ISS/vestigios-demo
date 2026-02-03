051c242a558bc0ccf906e5889f31ce9a2180d464
Julio David Benitez Rios <113691935+Mbarakaj4@users.noreply.github.com>
2026-02-03 14:16:50 -0300

    Please don't break it again G
    What changed:
    main_menu.gd - changed how the scene was being loaded.
    candle.tscn - fix textures
    detective.tscn - fix textures
    main_level.tsc - adjust overall map brightness

--- scenes/ui/screens/main_menu.gd
+++ scenes/ui/screens/main_menu.gd
@@ -1,8 +1,22 @@
 extends Control
 
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://scenes/main_level.tscn")
-
+	# Basicamente lo que hice aca es intentar no llamar la api
+	# Cada rato, por las dudas
+	var tree = get_tree()
+	
+	# Extraigo la escena actual para despues limpiar y pregargo el otro nivell
+	var c_scene = tree.get_current_scene()
+	var main_level = preload("res://scenes/main_level.tscn").instantiate()
+	
+	# Cargo el nodo en la raiz, quito la escena actual y le digo
+	# al engine que me cambie de escena
+	tree.get_root().add_child(main_level)
+	tree.get_root().remove_child(c_scene)
+	tree.set_current_scene(main_level)
+	
+	
+# codigo kk 
 func _on_credits_pressed():
 	get_tree().change_scene_to_file("res://scenes/ui/screens/credits.tscn")
 

8c1e6933a7addacfa84ef77869ad971813bf9dd2
Gabriel-ISS <77857318+Gabriel-ISS@users.noreply.github.com>
2026-02-08 20:08:40 -0300

    Feature character dialog (#6)
    * Fix: boton de play en menu principal
    
    * Elemento de dialog para el personaje
    
    * DialogTrigger para facilitar el diseño de niveles

--- scenes/ui/screens/main_menu.gd
+++ scenes/ui/screens/main_menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://scenes/main_level.tscn")
+	get_tree().change_scene_to_file("res://scenes/levels/level_1.tscn")
 
 func _on_credits_pressed():
 	get_tree().change_scene_to_file("res://scenes/ui/screens/credits.tscn")

4b93a08b0c92e6b37c46b1f360037b0546ede911
Cacike17 <marinrivasivandavid@gmail.com>
2026-02-12 14:05:54 -0300

    Pause and ost
    funcionalidad del menu de pausa junto con implementación en el nivel 1, agregue efectos de sonido a los botones de todas las interfaces y en menu y credits(use el efecto de viento como placeholder en este)

--- scenes/ui/screens/main_menu.gd
+++ scenes/ui/screens/main_menu.gd
@@ -1,10 +1,15 @@
 extends Control
 
+@onready var button_sound = $OptionsPadding/Options/buttonsound
+
 func _on_play_pressed():
+	button_sound.play()
 	get_tree().change_scene_to_file("res://scenes/levels/level_1.tscn")
 
 func _on_credits_pressed():
+	button_sound.play()
 	get_tree().change_scene_to_file("res://scenes/ui/screens/credits.tscn")
 
 func _on_exit_pressed():
+	button_sound.play()
 	get_tree().quit()

269ad60e760854b2f824017187bae06492b5fe4a
Gabriel-ISS <77857318+Gabriel-ISS@users.noreply.github.com>
2026-02-11 21:35:56 -0300

    Foondo para la caja de dialogo y ahora sigue correctamente al personaje sin ser aafectado por la luz (#9)

--- scenes/ui/components/dialog.gd
+++ scenes/ui/components/dialog.gd
@@ -1,7 +1,11 @@
-class_name Dialog extends VBoxContainer
+class_name Dialog extends CanvasLayer
 
-@onready var label: Label = $PanelContainer/MarginContainer/Label
+@onready var label: Label = $DialogBox/MarginContainer/MarginContainer/Label
 @onready var timer: Timer = $Timer
+@onready var parent: Node2D = get_parent()
+@onready var box: Control = $DialogBox
+
+@export var dialog_offset = Vector2(30, -90)
 
 var text:
 	set(value):
@@ -11,12 +15,20 @@ var text:
 
 func _ready() -> void:
 	visible = false
+	box.position = parent.global_position
+
+func _process(_delta: float) -> void:
+	var pos_local = dialog_offset
+	var transform_canvas = parent.get_global_transform_with_canvas()
+	var pos_canvas = transform_canvas * pos_local
+	
+	box.position.x = pos_canvas.x
+	box.position.y = pos_canvas.y - box.size.y
 
 func show_dialog(text: String, duration: float):
 	label.text = text
 	visible = true
 	timer.start(duration)
-
 
 func _on_timer_timeout() -> void:
 	visible = false

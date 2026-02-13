1efdb06b0aaa3255ad4936a95c80420a60b84796
pedrova096 <42945377+pedrova096@users.noreply.github.com>
2026-02-12 21:44:24 -0500

    Adelanto de nivel 1 (#11)

--- scenes/objects/detective.gd
+++ scenes/objects/detective.gd
@@ -4,9 +4,17 @@ class_name Character extends CharacterBody2D
 @onready var dead_screen: CanvasLayer = $DeadScreen
 @onready var flashlight: PointLight2D = $Candle
 @onready var dialog: Dialog = $Dialog
+@onready var character_camera: Camera2D = $Camera2D
 
 @export_category('Dependencies')
 @export var speed = 400
+
+@export_category('Camera')
+@export var enable_character_camera: bool = true:
+	set(value):
+		enable_character_camera = value
+		if is_node_ready():
+			_apply_character_camera_state()
 
 @export_category('Light offset')
 @export var offset_up: Vector2 = Vector2(0, -20)
@@ -20,6 +28,7 @@ func _ready():
 	dead_screen.visible = false
 	update_animation("idle")
 	update_flashlight()
+	_apply_character_camera_state()
 
 func _process(_delta):
 	handle_animation()
@@ -86,5 +95,48 @@ func update_flashlight():
 func _on_back_to_menu_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/ui/screens/main_menu.tscn")
 
-func show_dialog(text: String, duration: int):
-	dialog.show_dialog(text, duration)
+func configure_external_camera(external_camera: Camera2D) -> void:
+	if character_camera == null or external_camera == null:
+		return
+
+	_copy_camera_settings(character_camera, external_camera)
+
+func _apply_character_camera_state() -> void:
+	if character_camera == null:
+		return
+
+	character_camera.enabled = enable_character_camera
+	if enable_character_camera:
+		character_camera.make_current()
+
+func _copy_camera_settings(source_camera: Camera2D, target_camera: Camera2D) -> void:
+	var shared_properties: Array[StringName] = [
+		&"offset",
+		&"zoom",
+		&"ignore_rotation",
+		&"anchor_mode",
+		&"drag_horizontal_enabled",
+		&"drag_vertical_enabled",
+		&"drag_left_margin",
+		&"drag_right_margin",
+		&"drag_top_margin",
+		&"drag_bottom_margin",
+		&"drag_horizontal_offset",
+		&"drag_vertical_offset",
+		&"position_smoothing_enabled",
+		&"position_smoothing_speed",
+		&"rotation_smoothing_enabled",
+		&"rotation_smoothing_speed",
+		&"process_callback"
+	]
+
+	for property_name in shared_properties:
+		if _has_property(source_camera, property_name) and _has_property(target_camera, property_name):
+			target_camera.set(property_name, source_camera.get(property_name))
+
+func _has_property(target: Object, property_name: StringName) -> bool:
+	for property_data in target.get_property_list():
+		if property_data.name == property_name:
+			return true
+
+	return false

f1d6db40c81472c58e5d97006e6d0b0c8e24016a
Gabriel-ISS <gabrielsykora693@gmail.com>
2026-03-01 23:34:26 -0300

    Correcciones varias del menú de pausa

--- scenes/ui/screens/pause_menu.gd
+++ scenes/ui/screens/pause_menu.gd
@@ -1,51 +1,56 @@
-extends Control
+extends CanvasLayer
 
-@onready var buttonsound = $VBoxContainer/buttonsound
-@export var main_menu_scene: String = "res://MainMenu.tscn"
+@onready var button_sound: AudioStreamPlayer = $ButtonSound
+@onready var menu_container: PanelContainer = $MenuContainer
+@onready var pause_btn: TextureButton = $PauseBtn
+@onready var resume_btn: Button = $MenuContainer/ButtonsContainer/ResumeBtn
+@onready var main_menu_btn: Button = $MenuContainer/ButtonsContainer/MainMenuBtn
+@onready var exit_btn: Button = $MenuContainer/ButtonsContainer/ExitBtn
+
+@export var main_menu_scene: PackedScene
 
 func _ready():
 	# Comienza oculto
-	visible = false
+	menu_container.visible = false
 	# Permite procesar input aunque el juego esté pausado
 	process_mode = Node.PROCESS_MODE_ALWAYS
+	
+	# conectar señales
+	pause_btn.connect("pressed", pause)
+	resume_btn.connect("pressed", resume)
+	main_menu_btn.connect("pressed", go_to_main_menu)
+	exit_btn.connect("pressed", exit_game)
+
 
 func _unhandled_input(event):
 	# Presionando ESC activa o desactiva pausa
 	if event.is_action_pressed("ui_cancel"):
-		buttonsound.play()
-		if visible:
-			hide_pause()
+		if menu_container.visible:
+			resume()
 		else:
-			show_pause()
+			pause()
 
-func show_pause():
-	visible = true
+
+func pause():
+	button_sound.play()
+	pause_btn.visible = false
+	menu_container.visible = true
 	get_tree().paused = true
 
-func hide_pause():
-	visible = false
+
+func resume():
+	button_sound.play()
+	pause_btn.visible = true
+	menu_container.visible = false
 	get_tree().paused = false
 
-# Señales de botones
 
-func _on_resume_pressed():
-	buttonsound.play()
-	hide_pause()
-
-
-func _on_main_menu_pressed():
-	buttonsound.play()
+func go_to_main_menu():
+	button_sound.play()
 	get_tree().paused = false
-	get_tree().change_scene_to_file("res://scenes/ui/screens/main_menu.tscn")
+	get_tree().change_scene_to_packed(main_menu_scene)
 
 
-func _on_exit_pressed():
-	buttonsound.play()
+func exit_game():
+	button_sound.play()
 	get_tree().quit()
-
-
-func _on_pausebutton_pressed():
-	buttonsound.play()
-	var paused = get_tree().paused
-	get_tree().paused = !paused
-	visible = !paused

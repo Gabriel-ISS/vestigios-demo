90a08f20d9e34ab72ec5f7d14210aa7638a3bf57
Cacike17 <marinrivasivandavid@gmail.com>
2026-03-01 22:27:58 -0300

    Revert "Pausa y ost"

--- scenes/ui/screens/credits.gd
+++ scenes/ui/screens/credits.gd
@@ -1,7 +1,4 @@
 extends Control
 
-@onready var buttonsound = $buttonsound
-
 func _on_button_pressed() -> void:
-	buttonsound.play()
 	get_tree().change_scene_to_file("res://scenes/ui/screens/main_menu.tscn")
